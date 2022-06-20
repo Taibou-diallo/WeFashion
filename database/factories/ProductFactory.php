386ee0a514fe05469de5a39732752867f44ea538
Taibou DIALLO <t.diallo20170714@zig.univ.sn>
2022-06-20 23:37:23 +0200

    creation des factories et des seeders

---
+++ database/factories/ProductFactory.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Product>
+ */
+class ProductFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            //
+
+            'name' => $this->faker->name(),
+            'description' => $this->faker->sentence(),
+            'name' => $this->faker->name(),
+            'price' => $this->faker->randomFloat(5, 2),
+            'visibility' => $this->faker->randomElement([
+                'publish',
+                'no publish'
+            ]),
+            'reference' => $this->faker->sentence(),
+
+        ];
+    }
+}
---
+++ database/seeders/ProductTableSeeder.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Size;
+use App\Models\Product;
+use App\Models\Category;
+use Illuminate\Support\Str;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+class ProductTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+
+    public function run()
+    {
+        // création des categories
+        Category::factory()->create([
+            'name' => 'Homme'
+        ]);
+        Category::factory()->create([
+            'name' => 'Femme'
+        ]);
+
+
+        // creation des tailles
+        Size::factory()->create([
+            'name' => 'XS'
+        ]);
+        Size::factory()->create([
+            'name' => 'S'
+        ]);
+        Size::factory()->create([
+            'name' => 'M'
+        ]);
+        Size::factory()->create([
+            'name' => 'L'
+        ]);
+        Size::factory()->create([
+            'name' => 'XL'
+        ]);
+
+
+        // suprression des images avant les seeders
+        Storage::disk('local')->delete(Storage::allFiles());
+
+        //creation de 80 produits
+        Product::factory()->count(80)->create()->each(function ($product) {
+
+            $category = Category::find(rand(1, 2));
+
+            // on associe chaque produit a un categorie
+            $product->Category()->associate($category);
+
+            // sauvegarde de l'association
+            $product->save();
+
+            $folder = $product->category_id == 1 ? 'homme' : 'femme';
+
+            // ajout d'image
+            $link = Str::random(12) . '.jpg';
+            $file = file_get_contents(public_path('images/' . $folder . '/' . rand(1, 10) . 'jpg')); // flux
+            Storage::disk('local')->put($link, $file);
+
+            $product->picture()->create([
+                'title' => 'Default', // valeur par défaut
+                'link' => $link
+            ]);
+
+
+            $sizes = Size::pluck('id')->shuffle()->slice(0, rand(1, 5))->all();
+
+            // Il faut se mettre maintenant en relation avec les auteurs (relation ManyToMany) et attacher les id des auteurs
+            // dans la table de liaison.
+            $product->sizes()->attach($sizes); // ici on n'a pas besoin de faire persister c'est automatique dans une relation ManyToMany
+        });
+    }
+}
